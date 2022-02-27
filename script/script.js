8b52da19a40b8f8e01ee5c40200d9733c8213683
KEW18 <katewise18@gmail.com>
2022-02-27 17:34:14 +1100

    Assignment completed

---
+++ script/script.js
@@ -0,0 +1,71 @@
+let y=9;
+var message="hello";
+cars = ["jeep", "bmw", "volvo"];
+let favouriteNumber = 18;
+if (favouriteNumber >= 10) {favouriteNumber = favouriteNumber + 15}
+favouriteNumber;
+alert ('hello Kate');
+const first = 2;
+const second = 1;
+const problemCount = 99;
+const luckyNumber = (first > second) ? ( (problemCount - 1) / 2 ) : ( problemCount / 3 );
+luckyNumber; //=> ??? (Test it out yourself!)
+
+const msg1 = 'my String';
+const msg2 = "my String double";
+
+const birthDate = 30;
+const msg3 = `my age was ${birthDate}`;
+console.log(msg3);
+
+const firstName = "Byronius";
+const clanName = "Karbitus";
+const commonName = "Maris";
+let fullName;
+
+
+
+// Or, with interpolation
+fullName = `${firstName} ${clanName} ${commonName}`; //=> "Byronius Karbitus Maris"
+
+// Keep in mind it returns a _new_ String; therefore:
+firstName;  //=> "Byronius"
+clanName;   //=> "Karbitus"
+commonName; //=> "Maris"
+fullName;   //=> "Byronius Karbitus Maris"
+console.log(fullName);
+
+const num1=10, num2=5, num3=8;
+
+console.log (num1+num2);
+
+
+
+let firstVal = 10;
+let secondVal = 20;
+
+if(firstVal > secondVal)
+{
+    console.log("first value is greater");
+}
+
+if(firstVal < secondVal)
+{
+    console.log("first value is less");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
